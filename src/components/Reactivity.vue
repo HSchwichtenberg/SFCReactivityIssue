b8114693771998151e5cc97f837ec0ff6264a786
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2022-01-13 09:28:53 +0100

    first commit

---
+++ src/components/Reactivity.vue
@@ -0,0 +1,21 @@
+<!-- Vue.js Template -->
+<template>
+ <div id="app">
+   <div>x should be reactive: x={{x}}</div>
+    <div>y should NOT be reactive: y={{y }}</div>
+    <button @click="increment">Please click here</button> 
+  </div>
+</template>
+
+<script setup lang="ts">
+import {  ref } from 'vue';
+
+let x = ref(0);
+let y = 0; // no RefImpl
+
+function increment()
+{
+    x.value++; 
+    y++; 
+}
+</script>
